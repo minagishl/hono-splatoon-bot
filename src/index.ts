216376ae0460d9f2cfb52fddf0f8921ae04f9e85
minagishl <mvxd568.co@icloud.com>
2025-04-10 00:22:03 +0900

    feat: set up base logic structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import type { Context } from 'hono';
 import type { WebhookEvent } from '@line/bot-sdk';
 
 const app = new Hono();
@@ -15,7 +16,27 @@ app.post('/webhook', async (c) => {
 		return c.json({ message: 'Channel access token is not set' }, 500);
 	}
 
+	// Processes received events
+	c.executionCtx.waitUntil(processEvents(events, token, c));
+
 	return c.json({ message: 'Webhook received' }, 200);
 });
+
+async function processEvents(events: WebhookEvent[], token: string, c: Context) {
+	await Promise.all(
+		events.map(async (event) => {
+			try {
+				await console.log('test');
+			} catch (err) {
+				if (err instanceof Error) {
+					console.error(err);
+				}
+
+				// Handle error
+				return c.json({ message: 'Error processing event' }, 500);
+			}
+		})
+	);
+}
 
 Deno.serve(app.fetch);
