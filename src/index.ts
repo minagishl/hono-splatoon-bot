12237b337ca4a6b729c1bc14b377339bdd68b52b
minagishl <mvxd568.co@icloud.com>
2025-04-10 00:14:36 +0900

    feat: add base functionality for webhook

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono';
+import type { WebhookEvent } from '@line/bot-sdk';
 
 const app = new Hono();
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!');
+});
+
+app.post('/webhook', async (c) => {
+	const events: WebhookEvent[] = await c.req.json().then((data) => data.events);
+	const token = Deno.env.get('CHANNEL_ACCESS_TOKEN') ?? '';
+
+	if (token === '') {
+		return c.json({ message: 'Channel access token is not set' }, 500);
+	}
+
+	return c.json({ message: 'Webhook received' }, 200);
 });
 
 Deno.serve(app.fetch);
